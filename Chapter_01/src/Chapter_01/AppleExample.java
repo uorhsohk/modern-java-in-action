ed0912a13c0554790ef564ba1e26792c39b48caf
uorhsohk <msajjadkhoshrou@gmail.com>
2020-12-12 16:57:56 +0100

    streams API and parallel processing

--- Chapter_01/src/Chapter_01/AppleExample.java
+++ Chapter_01/src/Chapter_01/AppleExample.java
@@ -3,8 +3,10 @@ package Chapter_01;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static Chapter_01.COLOR.*;
+import static java.util.stream.Collectors.*;
 
 public class AppleExample {
     public static void main(String[] args) {
@@ -16,12 +18,24 @@ public class AppleExample {
         System.out.println(filterGreenApples(apples));
 
         // using method reference
-        System.out.println(filterApples(apples,Apple::isGreenApple));
+        System.out.println(filterApples(apples, Apple::isGreenApple));
         System.out.println(filterApples(apples, Apple::isHeavyApple));
 
         // using lambda
         System.out.println(filterApples(apples, (Apple a) -> GREEN.equals(a.getColor())));
         System.out.println(filterApples(apples, (Apple a) -> a.getWeight() > 100));
+
+        // using Stream API
+        List<Apple> heavyApples = apples.stream()
+                .filter((Apple a) -> a.getWeight() > 100)
+                .collect(toList());
+        System.out.println(heavyApples);
+
+        // using parallel processing
+        List<Apple> heavyApplesUsingParallelProcessing = apples.parallelStream()
+                .filter((Apple a) -> a.getWeight() > 100)
+                .collect(toList());
+        System.out.println(heavyApplesUsingParallelProcessing);
 
     }
 
