1b092b247de1c26fc706eebae293f951ebe358ab
uorhsohk <msajjadkhoshrou@gmail.com>
2020-12-13 14:20:47 +0100

    sorting using lambdas

--- Chapter_02/src/Behavior_Parameterization/AppleExample.java
+++ Chapter_02/src/Behavior_Parameterization/AppleExample.java
@@ -19,6 +19,15 @@ public class AppleExample {
         // using lambdas
         System.out.println(filterApplesBasedOn(inventory, (Apple a) -> a.getColor().equals(RED)));
 
+        // sorting using lambdas
+        List<Apple> sortInventoryByWeight = new ArrayList<>(inventory);
+        sortInventoryByWeight.sort((Apple a1, Apple a2) -> Integer.compare(a1.getWeight(), a2.getWeight()));
+        System.out.println(sortInventoryByWeight);
+
+        List<Apple> sortInventoryByColor = new ArrayList<>(inventory);
+        sortInventoryByColor.sort((Apple a1, Apple a2) -> a1.getColor().compareTo(a2.getColor()));
+        System.out.println(sortInventoryByColor);
+
     }
 
     public static List<Apple> filterApplesBasedOn(List<Apple> inventory, ApplePredicate predicate) {
